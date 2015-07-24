e03337c792fc783e5614f2d390fb30d61b733a2c
shizm <shizm@yonyou.com>
2015-07-24 08:24:31 +0800

    commit

--- src/main/java/com/shizm/dao/impl/UserDao.java
+++ src/main/java/com/shizm/dao/impl/UserDao.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 import com.shizm.dao.IUserDao;
